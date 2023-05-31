6fde7b7181acdea1ea5687c496605622e43673c4
Miguel Werneck <mig.roale@gmail.com>
2023-05-31 09:07:15 -0300

    converter e testes v1

---
+++ src/converter/converter.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def game_to_xml(game_data: pd.DataFrame):
+    game_df = pd.DataFrame(game_data)
+    xml_data = game_df.to_xml(root_name='data', row_name='row')
+    
+    with open('game_data.xml', 'w') as f:
+        f.write(xml_data)
+        
+def xml_to_game(xml_file):
+    game_df = pd.read_xml(xml_file)
+    return game_df
