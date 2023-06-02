b7628d39f21f17e013f2d0a24359d20804e6105d
Miguel Werneck <mig.roale@gmail.com>
2023-06-02 10:06:50 -0300

    game to xml ok, xml to game not

--- tests/converter/converter_test.py
+++ tests/converter/converter_test.py
@@ -2,22 +2,34 @@ import pandas as pd
 from src.converter.converter import *
 from src.game.game import *
 
-
-def test_game_to_xml():
-    game_data = [
-        {'id': 1, 'nome': 'Jogo 1', 'preco': 10.0, 'quantidade': 100},
-        {'id': 2, 'nome': 'Jogo 2', 'preco': 20.0, 'quantidade': 200},
-        {'id': 3, 'nome': 'Jogo 3', 'preco': 30.0, 'quantidade': 300},
+game_data = [
+        {'id': 1, 'nome': 'Jogo 1', 'preco': 10.0, 'quantidade': 100}
     ]
 
-    game_df = create_game_df(game_data)
-    game_to_xml(game_df)   # tem ainda que checar se o arquivo xml foi feito
+xml_data = """
+<?xml version='1.0' encoding='utf-8'?>
+<data>
+  <row>
+    <index>0</index>
+    <id>1</id>
+    <nome>Jogo 1</nome>
+    <preco>10.0</preco>
+    <quantidade>100</quantidade>
+  </row>
+</data>
+"""
+game_df = pd.DataFrame(game_data)
+
+def test_game_to_xml():
+    xml_str = game_to_xml(game_df)
+    assert isinstance(xml_str, str)
+    assert xml_str.strip().replace('\n', '') == xml_data.strip().replace('\n', '')
 
 
 def test_xml_to_game():
-    game_df = xml_to_game('game_data.xml')
+    game_df = xml_to_game(xml_data.strip().replace('\n', ''))
+    
     assert isinstance(game_df, pd.DataFrame)
-    assert len(game_df) == 3
-    assert game_df.loc[1, 'nome'] == 'Jogo 1'
-    assert game_df.loc[2, 'nome'] == 'Jogo 2'
-    assert game_df.loc[3, 'nome'] == 'Jogo 3'
+    assert game_df.equals(game_df)
+
+    
